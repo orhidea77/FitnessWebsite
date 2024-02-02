0ac0d39ceb2610072f1794d2c48ea5feb2ed572b
orhideaqadraku <orhideaaq@gmail.com>
2024-02-02 23:24:08 +0100

    user-php

---
+++ user.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>User Page</title>
+    <link rel="stylesheet" href="admin-user.css"/>
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="content">
+        <h3>Hi there <span>User</span></h3>
+        <h1>Welcome <span></span></h1>
+        <p>you are in the user page</p>
+        <a href="login.php" class="btn">Log In</a>
+        <a href="forma.php" class="btn">Register</a>
+        <a href="logout.php" class="btn">Log Out</a>
+      </div>
+    </div>
+
+</body>
+</html>
