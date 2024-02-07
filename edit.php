a8c428f08a2515a3f4e1a52b40f368121c9f5866
orhideaqadraku <orhideaaq@gmail.com>
2024-02-07 12:01:20 +0100

    edit php up

--- edit.php
+++ edit.php
@@ -1,140 +1,73 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "clients";
+require_once "database.php";
 
-$connection = new mysqli($servername, $username, $password, $database);
+if(isset($_POST['edit'])){
+    $eid = $_GET['editid'];
 
-$id = "";
-$name = "";
-$email = "";
-$number = "";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
 
-$errorMessage = "";
-$successMessage = "";
-
-if($_SERVER['REQUEST_METHOD']== 'GET'){
-    
-    if(!isset($_GET["id"])){
-        header("location: dashboard.php");
-        exit;
+    $sql = mysqli_query($conn, "UPDATE clients SET name = '$name', email = '$email', phone = '$phone' WHERE id = '$eid'");
+    if($sql){
+        echo "<script>alert('Successfull Edit');</script>";
+        echo "<script>document.location='dashboardd.php'</script>";
+    }else{
+        echo "<script>alert('Something went wrong')</script>";
     }
-
-    $id = $_GET["id"];
-
-    $sql = "SELECT * FROM clients WHERE id=$id";
-    $result = $connection->query($sql);
-    $row = $result->fetch_assoc();
-
-    if(!$row){
-        header("location:dashboard.php");
-        exit;
-    }
-
-    $name = $row["name"];
-    $email = $row["email"];
-    $number = $row["number"];
-}
-else{
-
-    $id = $_POST["id"];
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $number = $_POST["number"];
-
-    do{
-
-        if(empty($id) || empty($name) || empty($email) || empty($number)){
-            $errorMessage = "All the fields are required";
-        }
-
-        $sql = "UPDATE clients" . "SET name = '$name', email= '$email', number = '$number'" . "WHERE id = $id";
-
-        $result = $connection->query($sql);
-
-        if(!$result){
-            $errorMessage = "Invalid query " . $connection->error;
-            break;
-        }
-
-        $successMessage = "Client updated successfully";
-
-        header("location: dashboard.php");
-    }while(false);
 }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="db.css">
-
-	<title>Dashboard</title>
-</head>
-<body>
-	<div class="container">
-		<h2>New Client</h2>
-
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="d.css"/>
+  </head>
+  <body>
+<div class="container">
+  <div class="row">
+    <div class="column">
+      <h2>Edit Client</h2>
+    </div>
+  </div>
+      <form method="POST">
         <?php
-        if(!empty($errorMessage)){
-            echo "
-            <div class = 'alert alert-warning' role ='alert'>
-            <strong>$errorMessage</strong>
-            <button type='button' class='btn-close' label='Close'></button>
-            </div>
-            ";
-        }
+        $eid = $_GET['editid'];
+        $sql = mysqli_query($conn, "SELECT * FROM clients WHERE id='$eid'");
+        while($row=mysqli_fetch_array($sql)){
         ?>
-
-		<form method="post">
-            <input type="hidden" name= "id" value="<?php echo $id; ?>">
-			<div class="row">
-				<label class="colona">Name</label>
-				<div class="colon">
-					<input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
-				</div>
-			</div>
-			<div class="row">
-				<label class="colona">Email</label>
-				<div class="colon">
-					<input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
-				</div>
-			</div>
-			<div class="row">
-				<label class="colona">Number</label>
-				<div class="colon">
-					<input type="text" class="form-control" name="number" value="<?php echo $number; ?>">
-				</div>
-			</div>
-
-
-            <?php
-            if(!empty($successMessage)){
-                echo"
-                <div class = 'row'>
-                <div class = 'alert alert-success' role ='alert'>
-            <strong>$errorMessage</strong>
-            <button type='button' class='btn-close' label='Close'></button>
-            </div>
-            </div>
-            ";
-            }
-
-            ?>
-
-			<div class="row">
-				<div class="butoni">
-					<button type="submit" class="btn">Submit</button>
-				</div>
-				<div class="butoni">
-					<a class="btn" href="dashboard.php" role="button">Cancel</a>
-				</div>
-			</div>
-		</form>
-	</div>
-</body>
-</html>
+        <div class="row">
+        <div class="column">
+          <label>Name</label>
+          <input type="text" name="name" value="<?php echo $row['name'];?>"class="form-control" required>
+        </div>
+        </div>
+        <div class="row">
+        <div class="column">
+          <label>Email</label>
+          <input type="email" name="email" value="<?php echo $row['email'];?>" class="form-control" required>
+        </div>
+        </div>
+        <div class="row">
+        <div class="column">
+          <label for="phone">Phone</label>
+          <input type="text" name="phone" value="<?php echo $row['phone'];?>" class="form-control" required>
+        </div>
+        </div>
+        <?php
+        }?>
+        <div class="row">
+        <div class="column">
+          <button type="text" name="edit" class="btn">Add Client</button>
+          <a href="dashboardd.php">Registered Clients</a>
+        </div>
+        </div>
+      </form>
+    </div>
+    </body>
+    </html>
